c1acbb464b36d0f32ff2e21e8a392dbe37b6bc31
longtrinh <tenlong201@gmail.com>
2022-06-23 23:44:54 +0700

    Create Why-Hapolearn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,14 +17,18 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     @include('layouts.header')
     <main>
         @yield('content')
     </main>
+
     @include('layouts.course')
+    @include('layouts.reason')
     @include('layouts.footer')
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
