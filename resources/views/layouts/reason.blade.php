b71c530552ef417c04aa048628d6907c5a2d79c0
longtrinh <tenlong201@gmail.com>
2022-06-23 23:44:54 +0700

    Create Why-Hapolearn

---
+++ resources/views/layouts/reason.blade.php
@@ -0,0 +1,30 @@
+<div class="reason">
+    <div class="why">
+        <div class="why-tittle">why hapolearn?</div>
+        <div class="why-content">
+            <div class="why-list-reason">
+                <i class="fa-solid fa-circle-check"></i>
+                <p class="why-text">Interactive lessons, "on-the-go" practice, peer support.</p>
+            </div>
+            <div class="why-list-reason">
+                <i class="fa-solid fa-circle-check"></i>
+                <p class="why-text">Interactive lessons, "on-the-go" practice, peer support.</p>
+            </div>
+            <div class="why-list-reason">
+                <i class="fa-solid fa-circle-check"></i>
+                <p class="why-text">Interactive lessons, "on-the-go" practice, peer support.</p>
+            </div>
+            <div class="why-list-reason">
+                <i class="fa-solid fa-circle-check"></i>
+                <p class="why-text">Interactive lessons, "on-the-go" practice, peer support.</p>
+            </div>
+            <div class="why-list-reason">
+                <i class="fa-solid fa-circle-check"></i>
+                <p class="why-text">Interactive lessons, "on-the-go" practice, peer support.</p>
+            </div>
+        </div>
+    </div>
+    <div class="img-devices">
+        <img src="{{ asset('images/Devices.png') }}" class="img-devices-bg" alt="">
+        </div>
+</div>
