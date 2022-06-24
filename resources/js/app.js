b402cc95a998655d81b99e6168af92896d26df24
longtrinh <tenlong201@gmail.com>
2022-06-24 16:50:29 +0700

    Fix n times

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-// main.js
-import 'slick-carousel/slick/slick';
 
 require('./bootstrap');
+
 window.Vue = require('vue').default;
 
 /**
